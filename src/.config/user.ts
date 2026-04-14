5c158a068958247a049b0440243b35734648d976
Kmmoonlight <1501898512@qq.com>
2026-04-15 01:05:26 +0800

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,11 +1,71 @@
 import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
-  // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
-
-  socials: [],
-  themeStyle: 'system',
-  locale: 'zh-cn'
+  site: {
+    title: '学习笔记',
+    subtitle: 'Typography',
+    author: 'KMMoonlight',
+    description: '日常生活中的一些学习经验笔记',
+    website: 'https://wency.wang',
+    pageSize: 5,
+    socialLinks: [
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      }
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      }
+    ],
+    categoryMap: [{ name: '胡适', path: 'hu-shi' }],
+    footer: [
+      '© %year <a target="_blank" href="%website">%author</a>'
+    ],
+  },
+  appearance: {
+    theme: 'system',
+    locale: 'zh-cn',
+    colorsLight: {
+      primary: '#2e405b',
+      background: '#ffffff',
+    },
+    colorsDark: {
+      primary: '#FFFFFF',
+      background: '#232222',
+    },
+    fonts: {
+      header:
+        '"HiraMinProN-W6","Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
+      ui: '"Source Sans Pro","Roboto","Helvetica","Helvetica Neue","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+    },
+  },
+  seo: {
+    //twitter: '',
+    meta: [],
+    link: [],
+  },
+  rss: {
+    fullText: true,
+  },
+  comment: {
+    // disqus: { shortname: "typography-astro" },
+  },
+  analytics: {
+    googleAnalyticsId: '',
+    umamiAnalyticsId: '',
+  },
+  latex: {
+    katex: false,
+  },
 }
